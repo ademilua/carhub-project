34c0f77bc5e7d9e39c7ebb5fc4b630342b052402
maxybyte <tolesy2003@outlook.com>
2025-02-01 13:13:53 +0200

    create urls.py in accounts app and add some logic

---
+++ carhub/accounts/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+urlpatterns = [
+    # user should be able to register
+    # user should then be able to login
+    # user must be able to logout
+    # user must see the dashboard
+]
