6507b53c08b52d153454db56ded739ba8fa011ca
AimarToomela <toomelaaimar@gmail.com>
2025-02-01 13:40:14 +0200

    Added login in views.py

--- carhub/accounts/views.py
+++ carhub/accounts/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        
